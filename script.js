4ecff559586948ef07d7b6c0acbbfee4a9ed7afb
CristinaIftime <iftime.cristina20@gmail.com>
2024-04-10 11:05:38 -0700

    Added JS for show password

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var passwordInput = document.getElementById('password');
+    if (passwordInput.type === 'password') {
+        passwordInput.type = 'text';
+    } else {
+        passwordInput.type = 'password';
+    }
+}
