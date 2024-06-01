c6e2806f81514f57b1391c4919efd608e09ac3aa
CristinaIftime <iftime.cristina20@gmail.com>
2024-06-01 17:04:14 +0300

    The css are now used in .html

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,19 @@
 const fs = require('fs');
-
+const path = require('path');
+// Manage the css to be used in .html files
+function manageStyleCSS(req, res) {
+    const cssLocation = path.join(__dirname, '../public/css', path.basename(req.url));
+    fs.readFile(cssLocation, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('Cant find the css flise');
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.end(data);
+    });
+}
+// Manage the .html files to work with nodejs
 function manageHomeRoute (req, res) {
     fs.readFile('public/index.html', 'utf8', (err, data) => {
         if (err) {
@@ -14,6 +28,6 @@ function manageHomeRoute (req, res) {
 }
 
 module.exports = {
-    manageHomeRoute
-
+    manageHomeRoute,
+    manageStyleCSS
 };
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const http = require('http');
 const url = require('url');
-const { manageHomeRoute } = require('./routes/home');
+const { manageHomeRoute, manageStyleCSS } = require('./routes/home');
 
-
+// This manages the server
 const server = http.createServer((req, res) => {
   const urlParser = url.parse(req.url);
-
+// ## Points to / that is index.html
   if (urlParser.pathname === '/' && req.method === 'GET') {
     manageHomeRoute(req, res);
+// ##Finds the css files
+  } else if (urlParser.pathname.startsWith('/css/') && req.method === 'GET') {
+    manageStyleCSS(req, res);
     } else {
       res.writeHead(404, { 'Content-Type': 'text/plain' });
       res.end('Home page was not found');
